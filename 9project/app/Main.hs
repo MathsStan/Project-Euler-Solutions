0f1f810147d2b3aa307814a2810f47d7f7f705e9
MathsStan <stanleykitchen2000@gmail.com>
2023-01-26 21:02:58 +0000

    Added files for project 9.

--- 9project/app/Main.hs
+++ 9project/app/Main.hs
@@ -3,7 +3,10 @@ module Main where
 import Math.Combinat.Partitions.Integer (Partition, fromPartition, partitionsWithKParts)
 
 main :: IO ()
-main = putStrLn . show $ getPythagTrips 1000
+main = do
+    let triple = head $ getPythagTrips 1000
+    putStrLn . show $ triple
+    putStrLn . show $ product triple
 
 --a^2 + b^2 = c^2 -> a + b + c = n
 getPythagTrips :: Int -> [[Int]]
